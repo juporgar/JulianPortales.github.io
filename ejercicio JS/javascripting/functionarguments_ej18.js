6676fe6747b46a08ce322299f70ba6360bf2d3ec
juporgar <julian.porgar@gmail,com>
2018-05-08 16:35:54 +0200

    my teacher to the rescue

--- ejercicio JS/javascripting/functionarguments_ej18.js
+++
@@ -1,7 +0,0 @@
-function math (first,second,three){
-   return second * three + first;
-}
-
-math()
-
-console.log(math(53,61,67));
--- ejercicio JS/javascripting/functions_ej17.js
+++
@@ -1,5 +0,0 @@
-function eat (food) { 
-    return food + ' tasted really good.';
-}
-eat()
-console.log(eat('bananas'));
